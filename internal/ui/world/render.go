263d2386d71d31e3c50e274c17284fdd5b534d6d
Janith Leanage <janithleanage@gmail.com>
2025-03-21 22:08:07 +1300

    feat: added shore rendering

--- internal/ui/world/render.go
+++ internal/ui/world/render.go
@@ -10,9 +10,34 @@ import (
 
 // Renders the base tile
 func (wr *WorldRenderer) renderBaseTiles(screen *ebiten.Image, op *ebiten.DrawImageOptions, tiles [][]entities.Tile, x, y int) {
+	// Check neighbors (prevent out-of-bounds errors)
+	left, right, top, bottom := tiles[x][y].Elevation, tiles[x][y].Elevation, tiles[x][y].Elevation, tiles[x][y].Elevation
+	if x > 0 {
+		left = tiles[x-1][y].Elevation
+	}
+	if x < len(tiles)-1 {
+		right = tiles[x+1][y].Elevation
+	}
+	if y > 0 {
+		top = tiles[x][y-1].Elevation
+	}
+	if y < len(tiles[x])-1 {
+		bottom = tiles[x][y+1].Elevation
+	}
+
 	switch tiles[x][y].Elevation {
 	case entities.Sim.Geography.SeaLevel:
-		screen.DrawImage(assets.Assets.Sprites["sand"].Image, op)
+		if left == entities.Sim.Geography.SeaLevel+1 && right == entities.Sim.Geography.SeaLevel-1 {
+			screen.DrawImage(assets.Assets.Sprites["shore-x"].Image, op)
+		} else if left == entities.Sim.Geography.SeaLevel-1 && right == entities.Sim.Geography.SeaLevel+1 {
+			screen.DrawImage(assets.Assets.Sprites["shore-x-rev"].Image, op)
+		} else if top == entities.Sim.Geography.SeaLevel+1 && bottom == entities.Sim.Geography.SeaLevel-1 {
+			screen.DrawImage(assets.Assets.Sprites["shore-y"].Image, op)
+		} else if top == entities.Sim.Geography.SeaLevel-1 && bottom == entities.Sim.Geography.SeaLevel+1 {
+			screen.DrawImage(assets.Assets.Sprites["shore-y-rev"].Image, op)
+		} else {
+			screen.DrawImage(assets.Assets.Sprites["sand"].Image, op)
+		}
 	case 2:
 		screen.DrawImage(assets.Assets.Sprites["shallowwater"].Image, op)
 	case 1:
